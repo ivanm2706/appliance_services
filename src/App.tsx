1242ccc5bb9e493673e31e95d306247424f71c9e
Ivan Morhun <117114005+ivanm2706@users.noreply.github.com>
2023-09-26 22:46:52 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import './style/main.scss';
+import Header from './components/headers/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/headers/styles.d.ts
@@ -0,0 +1,6 @@
+declare const styles: {
+  readonly header: string;
+  readonly link: string;
+};
+
+export default styles;
